e19dfe2abd729163734b6583bc412bf227c0c23f
Morgan Sigland <morgan.sigland@gmail.com>
2026-05-08 10:07:29 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [{
+        provider: fontProviders.google(),
+        name: 'Inter',
+        cssVariable: '--font-inter',
+        weights: [400, 500, 600, 700],
+    }],
+
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ]
+    }
+});
